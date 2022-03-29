fc380c2bff77481c43b8e4394b3cddbdae0b5a8b
Victor Lin <linvictorw@gmail.com>
2022-03-29 19:35:06 +0800

    Add if statement and calc functions

--- calculator.go
+++ calculator.go
@@ -4,34 +4,54 @@ import "fmt"
 
 func add(a, b int) int {
 	//Insert code here
-
+	return a + b
 }
 
 func subtract(a, b int) int {
 	//Insert code here
-
+	return a - b
 }
 
 func multiply(a, b int) int {
 	//Insert code here
-
+	return a * b
 }
 
 func divide(a, b int) int {
 	//Insert code here
 	//consider for b = 0
-
+	if b != 0 {
+		return a / b
+	} else {
+		fmt.Println("Error!")
+		return 0
+	}
 }
 
 func main() {
 	var a, b int
 	var process string
-	fmt.Println("Enter an integer: ")
+	fmt.Printf("Enter an integer: ")
 	fmt.Scanln(&a)
-	fmt.Println("Enter process: (add, sub, mul, div)")
+	fmt.Printf("Enter process (add, sub, mul, div): ")
 	fmt.Scanln(&process)
-	fmt.Println("Enter an integer: ")
+	fmt.Printf("Enter an integer: ")
 	fmt.Scanln(&b)
 
 	//Insert code here
+	if process == "add" {
+		result := add(a, b)
+		fmt.Println("Result:", result)
+	} else if process == "sub" {
+		result := subtract(a, b)
+		fmt.Println("Result:", result)
+	} else if process == "mul" {
+		result := multiply(a, b)
+		fmt.Println("Result:", result)
+	} else if process == "div" && b != 0 {
+		result := divide(a, b)
+		fmt.Println("Result:", result)
+	} else {
+		fmt.Println("Error!")
+	}
 }
